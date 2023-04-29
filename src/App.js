8d05fc5b341cba304af1c1067563152c4e5808be
dev-deeplake <127172647+dev-deeplake@users.noreply.github.com>
2023-04-29 17:42:22 +0900

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const INITIAL_TODOLIST = []
+  const [writtenText, setWrittenText] = useState("")
+  const [todoList, setTodoList] = useState(INITIAL_TODOLIST)
+  const textTracker = (event) => setWrittenText(event.target.value)
+  const todoAdder = () => setTodoList([...todoList, writtenText])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="inputWrap">
+        <input type="text" onChange={textTracker}></input>
+        <button onClick={todoAdder}>추가하기</button>
+      </div>
+      <div className="todoListWrap">
+        <h1 className="todoList-title">Todo List</h1>
+        <TodoList list={todoList}/>
+      </div>
     </div>
   );
+}
+
+function TodoList ({list}) {
+  return (
+    <div className="todoList-entry">
+      {list.map(v => {return <TodoCard title={v} />})}
+    </div>
+  )
+}
+
+function TodoCard ({title}) {
+  return (
+    <div className="todo">
+      <p>{title}</p>
+    </div>
+  )
 }
 
 export default App;
